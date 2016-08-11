3ca4d5066e40de89bcaa140d53cd803643b66769
amandaderijk <amandaderijk@gmail.com>
2016-08-11 13:20:18 +0200

    continued on brick game
    only thing remaining is the management of the bricks and the increasing speed of the ball based on the duration of the game

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-window.onload = function(){
+(function(){
 	var canvas	= document.getElementById("game"),
 		game	= canvas.getContext("2d"),
-		paddlePosX = 400,
+		paddlePosX = 425,
 		paddleposY = 450,
+		paddleWidth = 150,
 		ballRadius = 10,
 		ballX = canvas.width/2,
-		ballY = canvas.height-30,
+		ballY = canvas.height - 70,
 		ballDX = 3,
-		ballDY = -3;
+		ballDY = -3,
+		ballInterval = 0;
+
+	var brickWidth = 150, 
+		brickHeight = 25, 
+		brickPosX = 100, 
+		brickPosY = 50;
+
 
 	// background
 	function drawBg(){
@@ -16,93 +24,114 @@ window.onload = function(){
 	}
 
 	// bricks 
+	// kan loop gebruiken eventueel om ze aan te maken en op afstand te meten
 	function drawBricks(){
 		game.fillStyle = "red";
-		game.fillRect (100, 50, 150, 25);
+		game.fillRect (100, 50, brickWidth, brickHeight);
 
 		game.fillStyle = "red";
-		game.fillRect (300, 50, 150, 25);
+		game.fillRect (300, 50, brickWidth, brickHeight);
 
 		game.fillStyle = "red";
-		game.fillRect (500, 50, 150, 25);
+		game.fillRect (500, 50, brickWidth, brickHeight);
 
 		game.fillStyle = "red";
-		game.fillRect (700, 50, 150, 25);
+		game.fillRect (700, 50, brickWidth, brickHeight);
 	}
 
 	// paddle
 	function drawPaddle(){
 		game.fillStyle = "darkblue";
-		game.fillRect (paddlePosX, paddleposY, 150, 25);
+		game.fillRect (paddlePosX, paddleposY, paddleWidth, 25);
 	}
 
 	// ball
 	function drawBall() {
     	game.beginPath();
-		game.arc(ballX, ballY, 10, 0, 2*Math.PI);
+		game.arc(ballX, ballY, ballRadius, 0, 2*Math.PI);
 		game.fillStyle = "black";
 		game.fill();
 		game.closePath();
 	}
 
 	// call the functions to get the elements visible
-	drawBg();
-	drawBricks();
-	drawPaddle();
-	drawBall();
-
-	// ball movement
-	// window.addEventListener("keydown", moveBall, true);
-	// space is key 32 
-	function moveBall() {
-		game.clearRect(0, 0, canvas.width, canvas.height);
+	function drawElements(){
 		drawBg();
 		drawBricks();
-		drawBall();
 		drawPaddle();
+		drawBall();
+	}
+	drawElements();
+	// ball movement
+	function moveBall() {
+		// console.log('move ball');
+		game.clearRect(0, 0, canvas.width, canvas.height);
+		drawElements();
+		bounceBall()
 
-		// check if ball is still in the canvas
-	    if(ballX + ballDX > canvas.width-ballRadius || ballX + ballDX < ballRadius) {
-	        ballDX = -ballDX;
-	    }
-	    if(ballY + ballDY < ballRadius || ballY + ballDY > canvas.height-ballRadius) {
-	        ballDY = -ballDY;
-	    } 
-	    // else if (ballY + ballDY > canvas.height-ballRadius) {
-	    // 	alert("Too bad..")
-	    // }
-	    // if (ballY + ballDY > paddleposY-ballRadius){
-	    // 	ballDY = -ballDY;
-	    // }
-
-	    //make ball move
+		// make ball move
 	    ballX += ballDX;
 	    ballY += ballDY;
-	}
-	setInterval(moveBall, 25); 	
 
+		// check if ball is still in the canvas
+	    if(ballX + ballDX > canvas.width - ballRadius || ballX + ballDX < ballRadius) {
+	        ballDX = -ballDX;
+	    }
+	    if(ballY + ballDY < ballRadius ) {
+	        ballDY = -ballDY;
+	    } 
+	}
+	// setInterval(moveBall, 25);
+
+	function bounceBall(){
+		// make the ball bounce on the paddle 
+	    if ((ballY + ballDY > paddleposY - ballRadius) && paddlePosX < ballX && ballX < paddlePosX + paddleWidth){
+	    	ballDY = -ballDY;
+	    }
+
+	    // make ball drop out of the game next to paddle 
+	    if (ballY + ballDY > canvas.height - ballRadius){
+		    console.log("Too bad..");
+
+		    // make ball stop & reset position
+		    ballX = canvas.width/2;
+			ballY = canvas.height - 70;
+
+			// reset paddle position
+			paddlePosX = 425;
+			paddleposY = 450;
+
+			clearInterval(ballInterval);
+			ballInterval = 0;
+			drawElements();
+	    }
+	}
 	// paddle movement
 	window.addEventListener("keydown", movePaddle, false);
 	function movePaddle(key){
 		switch (key.keyCode) {
-			case 37:
-				if (paddlePosX > 10){
+			case 37: //left arrow pressed
+				if (paddlePosX > 10){ // check if the paddle is still in the canvas
 					paddlePosX = paddlePosX - 15; //moves the paddle to the left 
 
-					drawPaddle();
+					drawElements();
 				} else {
 					paddlePosX = paddlePosX;
 				}
 				break;
-			case 39:
-				if (paddlePosX < 840){ // check if the paddle is still in the canvas
-					paddlePosX = paddlePosX + 15; //moves the paddle to the right 
+			case 39: //right arrow pressed
+				if (paddlePosX < 840){ 
+					paddlePosX = paddlePosX + 15;
 					
-					drawPaddle();
+					drawElements();
 				} else {
 					paddlePosX = paddlePosX;
 				}
+				break;
+			case 32:  //space bar pressed
+				clearInterval(ballInterval); //resets the current interval to stop the ball from accelerating. 
+				ballInterval = setInterval(moveBall, 25);
 				break;
 		}
 	}
-};
+})();
