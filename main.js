25d5cec1c1d37b7f6e98dbe5e38dcb30de556fc8
Amanda <amandaderijk@gmail.com>
2016-09-19 09:02:38 +0200

    Finished the game

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
 (function(){
 	var canvas	= document.getElementById("game"),
 		game	= canvas.getContext("2d"),
+
 		paddlePosX = 425,
 		paddleposY = 450,
 		paddleWidth = 150,
+		paddleHeight = 25,
+
 		ballRadius = 10,
 		ballX = canvas.width/2,
 		ballY = canvas.height - 70,
 		ballDX = 3,
 		ballDY = -3,
-		ballInterval = 0;
+		ballInterval = 0,
 
-	var brickWidth = 150, 
+		brickWidth = 150, 
 		brickHeight = 25, 
-		brickPosX = 100, 
-		brickPosY = 50;
+		brickPosXStart = 70, 
+		brickPosYStart = 50,
+		brickAmount = 8,
+		brickHit = false,
+		brickColor= "red",
+		brickColorHit = "lightgray",
+		bricks = [],
 
+		score = 0,
+
+		gameOver = false;
+		
 
 	// background
 	function drawBg(){
@@ -24,25 +36,41 @@
 	}
 
 	// bricks 
-	// kan loop gebruiken eventueel om ze aan te maken en op afstand te meten
 	function drawBricks(){
-		game.fillStyle = "red";
-		game.fillRect (100, 50, brickWidth, brickHeight);
+		var brickPosX = brickPosXStart, // 70
+			brickPosY = brickPosYStart;
 
-		game.fillStyle = "red";
-		game.fillRect (300, 50, brickWidth, brickHeight);
+		for (var i = 0; i <= brickAmount; i++) {
+			game.beginPath();
+			game.fillStyle = brickColor;
+			game.fillRect (brickPosX, brickPosY, brickWidth, brickHeight);
+			game.closePath();
+			
+			// push brick positions as an object into an array
+			bricks.push({brickPosX, brickPosY, brickColor, brickHit});
+			// console.log(bricks);
+			if (bricks[i].brickHit){
+				game.beginPath();
+				game.fillStyle = brickColorHit;
+				game.fillRect (brickPosX, brickPosY, brickWidth, brickHeight);
+				game.closePath();
+			};			
+			
+			brickPosX += 175;
 
-		game.fillStyle = "red";
-		game.fillRect (500, 50, brickWidth, brickHeight);
-
-		game.fillStyle = "red";
-		game.fillRect (700, 50, brickWidth, brickHeight);
+			// als binnen breedte = brickPosY + brick height + 50
+			if (brickPosX + brickWidth >= canvas.width){
+				brickPosY += 50;
+				brickPosX = 157;
+			}
+		}
+		// console.log(bricks);
 	}
 
 	// paddle
 	function drawPaddle(){
 		game.fillStyle = "darkblue";
-		game.fillRect (paddlePosX, paddleposY, paddleWidth, 25);
+		game.fillRect (paddlePosX, paddleposY, paddleWidth, paddleHeight);
 	}
 
 	// ball
@@ -54,17 +82,24 @@
 		game.closePath();
 	}
 
+	// get lives and score on the screen
+	function drawscore(){
+		game.font="18px Verdana";
+		game.fillText ("score: " + score, canvas.width - 100, 25);
+	}
+
 	// call the functions to get the elements visible
 	function drawElements(){
 		drawBg();
 		drawBricks();
 		drawPaddle();
 		drawBall();
+		drawscore();
 	}
 	drawElements();
+
 	// ball movement
 	function moveBall() {
-		// console.log('move ball');
 		game.clearRect(0, 0, canvas.width, canvas.height);
 		drawElements();
 		bounceBall()
@@ -73,39 +108,68 @@
 	    ballX += ballDX;
 	    ballY += ballDY;
 
-		// check if ball is still in the canvas
+		// reverse direction if ball hits border of canvas
 	    if(ballX + ballDX > canvas.width - ballRadius || ballX + ballDX < ballRadius) {
-	        ballDX = -ballDX;
+	        ballDX =- ballDX;
 	    }
 	    if(ballY + ballDY < ballRadius ) {
-	        ballDY = -ballDY;
+	        ballDY =- ballDY;
 	    } 
+		// check if the ball has hit the brick.
+		for (var i = 0; i <= brickAmount; i++) {
+			if(ballX + ballDX > bricks[i].brickPosX - ballRadius && 
+				ballX + ballDX < bricks[i].brickPosX + brickWidth - ballRadius && 
+				ballY + ballDY > bricks[i].brickPosY - ballRadius &&
+			 	ballY + ballDY < bricks[i].brickPosY + brickHeight - ballRadius
+			 	&& !bricks[i].brickHit){
+				
+				bricks[i].brickHit = true;
+				score += 1; // punten omhoog als geraakt
+			}
+		}
 	}
-	// setInterval(moveBall, 25);
 
 	function bounceBall(){
 		// make the ball bounce on the paddle 
-	    if ((ballY + ballDY > paddleposY - ballRadius) && paddlePosX < ballX && ballX < paddlePosX + paddleWidth){
-	    	ballDY = -ballDY;
+	    if ( ballY + ballDY > paddleposY - ballRadius 
+	    	&& paddlePosX - ballRadius < ballX 
+	    	&& ballX < paddlePosX + paddleWidth + ballRadius
+	    	&& !gameOver){
+	    	ballDY = -ballDY * 1.2;
+
 	    }
 
 	    // make ball drop out of the game next to paddle 
-	    if (ballY + ballDY > canvas.height - ballRadius){
-		    console.log("Too bad..");
+	    if (ballY + ballDY > paddleposY){
+		    gameOver = true;
+		}
 
-		    // make ball stop & reset position
-		    ballX = canvas.width/2;
+	    // make ball stop & reset position
+	    if(ballY + ballDY > canvas.height){
+			ballX = canvas.width/2;
 			ballY = canvas.height - 70;
 
 			// reset paddle position
 			paddlePosX = 425;
 			paddleposY = 450;
 
+			// reset ball direction
+			ballX += ballDX;
+	    	ballY += ballDY;
+
+			for (var i = 0; i <= brickAmount; i++){
+				bricks[i].brickHit = false;
+			}
+			score = 0;
+			ballDY = -3;
+
+			alert("Too bad..");
 			clearInterval(ballInterval);
 			ballInterval = 0;
 			drawElements();
-	    }
+		}
 	}
+
 	// paddle movement
 	window.addEventListener("keydown", movePaddle, false);
 	function movePaddle(key){
